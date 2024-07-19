799f117a6e7090fee6bee2ff71568b918f19b97e
sunnyshah-23 <shahsunny311@gmail.com>
2024-07-19 00:24:53 -0400

    changed firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     firebase:{
-        apiKey: "AIzaSyDup4BETbndYUnDhFQUbLY1RiwvGPd0LzQ",
-        authDomain: "flight-reservation-41bde.firebaseapp.com",
-        projectId: "flight-reservation-41bde",
-        storageBucket: "flight-reservation-41bde.appspot.com",
-        messagingSenderId: "566488701222",
-        appId: "1:566488701222:web:6f9989e72afa2715dc49e7"
+        apiKey: "AIzaSyA2Mx-33iVpseAdmmEqfvumiJAzl1j8mmE",
+        authDomain: "hotel-booking-b2a86.firebaseapp.com",
+        projectId: "hotel-booking-b2a86",
+        storageBucket: "hotel-booking-b2a86.appspot.com",
+        messagingSenderId: "347911253617",
+        appId: "1:347911253617:web:d63c2bf731f8f083b6b5d0"
     }
     
   };
